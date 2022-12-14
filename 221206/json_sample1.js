1b2a49164f2d6e8fd038d14cb707e5e5e58527d6
saito1022 <m0a20128a7@edu.teu.ac.jp>
2022-12-14 12:58:13 +0900

    てつのりです

--- 221206/json_sample1.js
+++ 221206/json_sample1.js
@@ -1,10 +1,10 @@
-let url ="https://morimorihoge.github.io/teu2022f/tweets.json";
+let url = "https://morimorihoge.github.io/teu2022f/tweets.json";
 
 fetch(url)
     .then((response) => response.json())
     .then(
         function (json) {
-            console.log("ボタンを押してね");
+            console.log("工事中です");
             let answerHtml = "";
             json.forEach(function (tweet) {
                 answerHtml += `<li><b>${tweet.name}</b>: <i>${tweet.message}</i> <i>${tweet.tweetedAt}</i></li>`;
@@ -27,22 +27,64 @@ function ALL() {
             }
         )
 }
-
-function sabu() {
+function Sabu() {
     fetch(url)
         .then((response) => response.json())
         .then(
             function (json) {
-                console.log("sabu");
+                console.log("Sabu");
                 let answerHtml = "";
-                if (json.name === "三郎BOT") {
-                    json.name.forEach(function (tweet) {
+                console.log(json);
+                console.log(json[0].name);
+                json.forEach(function (tweet) {
+                    console.log(tweet);
+                    if (tweet.name === "三郎BOT") {
                         answerHtml += `<li><b>${tweet.name}</b>: <i>${tweet.message}</i> <i>${tweet.tweetedAt}</i></li>`;
-                    });
-                    document.querySelector("#answer").innerHTML = answerHtml
-                }
+                    }
+
+                    document.querySelector("#answer").innerHTML = answerHtml;
+                })
             }
         )
 }
+function Ziro() {
+    fetch(url)
+        .then((response) => response.json())
+        .then(
+            function (json) {
+                console.log("Ziro");
+                let answerHtml = "";
+                console.log(json);
+                console.log(json[0].name);
+                json.forEach(function (tweet) {
+                    console.log(tweet);
+                    if (tweet.name === "次郎") {
+                        answerHtml += `<li><b>${tweet.name}</b>: <i>${tweet.message}</i> <i>${tweet.tweetedAt}</i></li>`;
+                    }
+                    document.querySelector("#answer").innerHTML = answerHtml;
+                })
+            }
+        )
+}
+function Taro() {
+    fetch(url)
+        .then((response) => response.json())
+        .then(
+            function (json) {
+                console.log("Taro");
+                let answerHtml = "";
+                console.log(json);
+                console.log(json[0].name);
+                json.forEach(function (tweet) {
+                    console.log(tweet);
+                    if (tweet.name === "太郎") {
+                        answerHtml += `<li><b>${tweet.name}</b>: <i>${tweet.message}</i> <i>${tweet.tweetedAt}</i></li>`;
+                    }
+                    document.querySelector("#answer").innerHTML = answerHtml;
+                })
+            }
+        )
+}
+
 
 
