23c7959d95d1d332d977d72c79da50c5b5da635c
saito1022 <m0a20128a7@edu.teu.ac.jp>
2022-12-30 02:04:42 +0900

    てつのりです

---
+++ assessment_nameShindan/assessment.js
@@ -0,0 +1,31 @@
+console.log('テスト');
+'use strict';
+const answers =[
+    '{userName}のいいところは声です。{userName}の特徴的な声はみなを引き付け、心に残ります。',
+    '{userName}のいいところはまなざしです。{userName}に見つめられた人は、気になって仕方がないでしょう。',
+    '{userName}のいいところは情熱です。',
+    '{userName}のいいところは厳しさです。',
+    '{userName}のいいところは知識です。',
+    '{userName}のいいところはユニークさです。',
+    '{userName}のいいところは用心深さです。',
+    '{userName}のいいところは見た目です。',
+    '{userName}のいいところは決断力です。',
+    '{userName}のいいところは思いやりです。',
+    '{userName}のいいところは感受性です。',
+    '{userName}のいいところは節度です。',
+    '{userName}のいいところは好奇心です。',
+    '{userName}のいいところは気配りです。',
+    '{userName}のいいところはその全てです。',
+    '{userName}のいいところは自制心です。',
+]
+/**
+ * 名前の文字列を渡すと診断結果を返す関数
+ * @param {string} userName ユーザーの名前
+ * @return {string} 診断結果
+ * (param = parameter(パラメータ) = 関数の「引数」)
+ *
+ */
+function assessment(userName){
+    //TODO 診断処理を実装する
+    return'';
+}
