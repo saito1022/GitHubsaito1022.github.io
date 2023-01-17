db1de74b59d7557a88a835ad666be89ed4ea0273
saito1022 <m0a20128a7@edu.teu.ac.jp>
2023-01-17 23:33:13 +0900

    てつのりです

---
+++ final/final.js
@@ -0,0 +1,35 @@
+const ITEMS_CONTAINER = document.getElementById("items");
+const ITEMS_TEMPLATE = document.getElementById("itemTemplate");
+const ADD_BUTTON = document.getElementById("add");
+
+let items = getItems();
+
+function getItems(){
+    const value = localStorage.getItem("todo-test")||"[]";
+
+    return JSON.parse(value);
+
+}
+function setItems(items){
+    const itemsJson = JSON.stringify(items);
+    localStorage.setItem("todo-test",itemsJson);
+}
+function addItem(){
+    items.unshift({
+        description:"",
+        completed: false
+    });
+
+    setItems(items);
+    refreshList();
+}
+function refreshList(){
+    // todo sort items
+    ITEMS_CONTAINER.innerHTML = "";
+
+    for(const item of items){
+        const itemElement = ITEM_TEMPLATE.content.cloneNode(true);
+        const descriptionInput = itemElement.querySelector(".item-description");
+    }
+}
+console.log(items);
